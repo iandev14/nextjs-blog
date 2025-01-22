a6de524f829c81d825efca55d329b609187ae430
ian <villanuevachristiandel@gmail.com>
2025-01-22 09:44:13 +0800

    add custom css component and add dashboard page

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,9 @@
+export default function dashboardLayout({ children }) {
+    return (
+        <div>
+            <h1>Dashboard Layout</h1>
+
+            <div>{children}</div>
+        </div>
+    );
+}
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
@@ -5,7 +6,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <header>
-          <nav>Nav</nav>
+          <nav>
+          <Link href="/dashboard" className="nav-link">Home</Link>
+            <Link href="/dashboard" className="nav-link">Dashboard</Link>
+          </nav>
         </header>
         <main>{children}</main>
         <footer>footer</footer>
